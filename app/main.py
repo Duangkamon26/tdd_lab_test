a62d1f0077ece03f667c8aecaa1abb383da73a6a
Duangkamon26 <115264230+Duangkamon26@users.noreply.github.com>
2023-03-22 16:10:25 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -8,7 +8,7 @@ def read_root():
     return {"Hello": "World"}
 @app.post("/")
 def read_root():
-    return {"Hello": "WorldTesttwo"}
+    return {"Hello": "World"}
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
